bef254c67c99d7b5dfb79c9145041edba967c615
Mitch Tabian <mitch@tabian.ca>
2018-07-18 12:12:15 -0700

    start

--- app/src/main/java/com/jwhh/notekeeper/SettingsActivity.kt
+++ app/src/main/java/com/jwhh/notekeeper/SettingsActivity.kt
@@ -1,8 +1,12 @@
 package com.jwhh.notekeeper
 
+import android.app.FragmentTransaction
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.preference.Preference
 import android.preference.PreferenceActivity
+import android.preference.PreferenceFragment
+import android.provider.Settings
 
 class SettingsActivity: PreferenceActivity()
 {
@@ -10,8 +14,13 @@ class SettingsActivity: PreferenceActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        addPreferencesFromResource(R.xml.pref_main)
+//        addPreferencesFromResource(R.xml.pref_main)
     }
+
+    override fun onBuildHeaders(target: MutableList<Header>?) {
+        loadHeadersFromResource(R.xml.preference_headers, target)
+    }
+    
 }
 
 
