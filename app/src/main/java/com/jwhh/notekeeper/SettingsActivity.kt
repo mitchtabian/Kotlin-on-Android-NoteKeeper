ccb5e7fd19c3a1f460e586b76c48f814f6aaca3b
Mitch Tabian <mitch@tabian.ca>
2018-07-18 14:28:12 -0700

    end

--- app/src/main/java/com/jwhh/notekeeper/SettingsActivity.kt
+++ app/src/main/java/com/jwhh/notekeeper/SettingsActivity.kt
@@ -7,6 +7,7 @@ import android.preference.Preference
 import android.preference.PreferenceActivity
 import android.preference.PreferenceFragment
 import android.provider.Settings
+import android.util.Log
 
 class SettingsActivity: PreferenceActivity()
 {
@@ -22,8 +23,11 @@ class SettingsActivity: PreferenceActivity()
         return false
     }
 
-    class TabletSettingsFragment : PreferenceFragment()
+    class TabletSettingsFragment : PreferenceFragment(),
+            Preference.OnPreferenceChangeListener
     {
+
+        val TAG = "TabletSettingsFragment"
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
@@ -31,15 +35,68 @@ class SettingsActivity: PreferenceActivity()
             val settings = arguments.getString("settings")
             if(settings.equals(getString(R.string.preferences_notifications))){
                 addPreferencesFromResource(R.xml.preference_notifications)
+
+                val notificationsNewMessagePreference: Preference = preferenceManager.findPreference(getString(R.string.key_notifications_new_message))
+                notificationsNewMessagePreference.setOnPreferenceChangeListener(this)
+
+                val notificationsRingtonePreference: Preference = preferenceManager.findPreference(getString(R.string.key_notifications_new_message_ringtone))
+                notificationsRingtonePreference.setOnPreferenceChangeListener(this)
+
+                val vibratePreference: Preference = preferenceManager.findPreference(getString(R.string.key_vibrate))
+                vibratePreference.setOnPreferenceChangeListener(this)
             }
             else if(settings.equals(getString(R.string.preferences_sync))){
                 addPreferencesFromResource(R.xml.preference_sync)
+
+                // set Preference Change Listeners
+                val galleryNamePreference: Preference = preferenceManager.findPreference(getString(R.string.key_gallery_name))
+                galleryNamePreference.setOnPreferenceChangeListener(this)
+
+                val uploadWifiPreference: Preference = preferenceManager.findPreference(getString(R.string.key_upload_over_wifi))
+                uploadWifiPreference.setOnPreferenceChangeListener(this)
+
+                val backupFrequencyPreference: Preference = preferenceManager.findPreference(getString(R.string.key_backup_frequency))
+                backupFrequencyPreference.setOnPreferenceChangeListener(this)
             }
             else if(settings.equals(getString(R.string.preferences_about))){
                 addPreferencesFromResource(R.xml.preference_about)
             }
         }
+
+        override fun onPreferenceChange(p0: Preference?, key: Any?): Boolean {
+            printToLog("Preference change detected")
+            when(key){
+                getString(R.string.key_gallery_name) -> updatePreferenceSuccess(getString(R.string.key_gallery_name))
+                getString(R.string.key_upload_over_wifi) -> updatePreferenceSuccess(getString(R.string.key_upload_over_wifi))
+                getString(R.string.key_notifications_new_message) -> updatePreferenceSuccess(getString(R.string.key_notifications_new_message))
+                getString(R.string.key_notifications_new_message_ringtone) -> updatePreferenceSuccess(getString(R.string.key_notifications_new_message_ringtone))
+                getString(R.string.key_vibrate) -> updatePreferenceSuccess(getString(R.string.key_vibrate))
+                getString(R.string.key_backup_frequency) -> updatePreferenceSuccess(getString(R.string.key_backup_frequency))
+            }
+
+            return true // Update the state of the preference with the new value
+        }
+
+        fun updatePreferenceSuccess(key: String?){
+
+            // If this was a real application we would send the updates to server here
+            uploadPreferencesToServer()
+
+            printToLog("successfully updated preferences. key: " + key)
+
+        }
+
+        private fun uploadPreferencesToServer(){
+            // Code for uploading updated preferences to server
+        }
+
+        private fun printToLog(message: String?){
+            Log.d(TAG, message)
+        }
+
     }
+
+
 }
 
 
--- app/src/main/java/com/jwhh/notekeeper/SettingsFragment.kt
+++ app/src/main/java/com/jwhh/notekeeper/SettingsFragment.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import android.preference.Preference
 import android.preference.PreferenceFragment
 import android.support.v4.content.ContextCompat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 class SettingsFragment: PreferenceFragment(),
-        Preference.OnPreferenceClickListener
+        Preference.OnPreferenceClickListener,
+        Preference.OnPreferenceChangeListener
 {
+
+    val TAG = "SettingsFragment"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,24 @@ class SettingsFragment: PreferenceFragment(),
         val accountPreference: Preference = findPreference(getString(R.string.key_account_settings))
         accountPreference.setOnPreferenceClickListener {onPreferenceClick(it)}
 
+        // set Preference Change Listeners
+        val galleryNamePreference: Preference = preferenceManager.findPreference(getString(R.string.key_gallery_name))
+        galleryNamePreference.setOnPreferenceChangeListener(this)
+
+        val uploadWifiPreference: Preference = preferenceManager.findPreference(getString(R.string.key_upload_over_wifi))
+        uploadWifiPreference.setOnPreferenceChangeListener(this)
+
+        val notificationsNewMessagePreference: Preference = preferenceManager.findPreference(getString(R.string.key_notifications_new_message))
+        notificationsNewMessagePreference.setOnPreferenceChangeListener(this)
+
+        val notificationsRingtonePreference: Preference = preferenceManager.findPreference(getString(R.string.key_notifications_new_message_ringtone))
+        notificationsRingtonePreference.setOnPreferenceChangeListener(this)
+
+        val vibratePreference: Preference = preferenceManager.findPreference(getString(R.string.key_vibrate))
+        vibratePreference.setOnPreferenceChangeListener(this)
+
+        val backupFrequencyPreference: Preference = preferenceManager.findPreference(getString(R.string.key_backup_frequency))
+        backupFrequencyPreference.setOnPreferenceChangeListener(this)
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -29,6 +51,20 @@ class SettingsFragment: PreferenceFragment(),
         view.setBackgroundColor(ContextCompat.getColor(activity, R.color.white))
 
         return view
+    }
+
+    override fun onPreferenceChange(preference: Preference?, key: Any?): Boolean {
+        printToLog("Preference change detected")
+        when(key){
+            getString(R.string.key_gallery_name) -> updatePreferenceSuccess(getString(R.string.key_gallery_name))
+            getString(R.string.key_upload_over_wifi) -> updatePreferenceSuccess(getString(R.string.key_upload_over_wifi))
+            getString(R.string.key_notifications_new_message) -> updatePreferenceSuccess(getString(R.string.key_notifications_new_message))
+            getString(R.string.key_notifications_new_message_ringtone) -> updatePreferenceSuccess(getString(R.string.key_notifications_new_message_ringtone))
+            getString(R.string.key_vibrate) -> updatePreferenceSuccess(getString(R.string.key_vibrate))
+            getString(R.string.key_backup_frequency) -> updatePreferenceSuccess(getString(R.string.key_backup_frequency))
+        }
+
+        return true // Update the state of the preference with the new value
     }
 
     override fun onPreferenceClick(preference: Preference?): Boolean {
@@ -39,6 +75,23 @@ class SettingsFragment: PreferenceFragment(),
         }
 
         return true
+    }
+
+    fun updatePreferenceSuccess(key: String?){
+
+        // If this was a real application we would send the updates to server here
+        uploadPreferencesToServer()
+
+        printToLog("successfully updated preferences. key: " + key)
+
+    }
+
+    private fun uploadPreferencesToServer(){
+        // Code for uploading updated preferences to server
+    }
+
+    private fun printToLog(message: String?){
+        Log.d(TAG, message)
     }
 }
 
